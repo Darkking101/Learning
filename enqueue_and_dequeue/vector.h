fdfb9381894d80359a8bee297c278de0d360e224
Vasu0521c <vasudevanoffline@hotmail.com>
2026-06-20 14:18:48 +0530

    enqueue and dequeue

---
+++ enqueue_and_dequeue/vector.h
@@ -0,0 +1,24 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+typedef struct {
+
+	int *data;
+	int length;
+	int size;
+} vectar;
+
+
+vectar* create_vectar();
+vectar* create_vectar_with_size(int size);
+void clear(vectar *vec);
+void pop(vectar *vec);
+void push(vectar *vec, int data);
+int compare(vectar *vec_a, vectar *vec_b);
+vectar* reverse(vectar *vec);
+vectar* clone(vectar *vec);
+void insert_at(vectar *vec, int index, int data);
+void remove_at(vectar *vec, int index);
+void display(vectar *vec);
+
+#endif
